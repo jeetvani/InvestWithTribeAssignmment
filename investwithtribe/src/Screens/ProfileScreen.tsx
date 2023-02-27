029998d1dafad9dc7d39967afeb2ffd0322bb0e5
Jeet Vani <jeetvani@Jeets-MacBook-Pro.local>
2023-02-28 00:27:41 +0530

    Update

--- investwithtribe/src/Screens/ProfileScreen.tsx
+++ investwithtribe/src/Screens/ProfileScreen.tsx
@@ -1,50 +1,66 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import PrimaryButton from '../Components/PrimaryButton';
+
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
 
-interface ProfileScreenProps { }
+import PrimaryButton from '../Components/PrimaryButton';
+import axiosClient from '../Api/axiosClient';
+
+interface UserData {
+  Name: string;
+  PAN: string;
+  Email: string;
+  Address: string;
+}
+
+interface ProfileScreenProps {}
 
 const ProfileScreen: React.FC<ProfileScreenProps> = () => {
-
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const navigation = useNavigation();
 
   const getUserDetails = async () => {
-    const Id = await AsyncStorage.getItem('Id');
-    const response = await axiosClient.post('/getUserDetails', {
-      Id,
+    const id = await AsyncStorage.getItem('Id');
+    const response = await axiosClient.post<{ userData: UserData }>('/LoginUser', {
+      Id: id,
     });
-    const { Name, PAN, Email, Address } = response.data.userData;
-    setName(Name);
-    setPAN(PAN);
-    setEmail(Email);
-    setAddress(Address);
-  }
+    setUserData(response.data.userData);
+  };
+
   useEffect(() => {
     getUserDetails();
   }, []);
 
-  const navigation = useNavigation()
-  const [name, setName] = useState('');
-  const [pan, setPAN] = useState('');
-  const [email, setEmail] = useState('');
-  const [address, setAddress] = useState('');
-  const LogOut = async () => {
+  const handleLogout = async () => {
     await AsyncStorage.removeItem('Id');
     navigation.navigate('Auth');
+  };
+
+  if (!userData) {
+    return null; // or render a loader
   }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Profile</Text>
-      <Text style={styles.label}>Name:</Text>
-      <Text style={styles.value}>{name}</Text>
-      <Text style={styles.label}>PAN:</Text>
-      <Text style={styles.value}>{pan}</Text>
-      <Text style={styles.label}>Email:</Text>
-      <Text style={styles.value}>{email}</Text>
-      <Text style={styles.label}>Address:</Text>
-      <Text style={styles.value}>{address}</Text>
-      <PrimaryButton onPress={LogOut} content='Log Out' />
+      <View style={styles.row}>
+        <Text style={styles.label}>Name:</Text>
+        <Text style={styles.value}>{userData.Name}</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.label}>PAN:</Text>
+        <Text style={styles.value}>{userData.PAN}</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.label}>Email:</Text>
+        <Text style={styles.value}>{userData.Email}</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.label}>Address:</Text>
+        <Text style={styles.value}>{userData.Address}</Text>
+      </View>
+      <PrimaryButton onPress={handleLogout} content='Log Out' />
     </View>
   );
 };
@@ -59,16 +75,20 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
     marginBottom: 10,
   },
   label: {
     fontSize: 16,
     fontWeight: 'bold',
-    marginTop: 10,
+    marginRight: 10,
   },
   value: {
     fontSize: 16,
-    marginBottom: 10,
   },
 });
 
